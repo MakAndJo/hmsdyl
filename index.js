f843f113cfb21c37109fb768a8c763081a685840
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2023-02-09 00:36:33 +0500

    added share button

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const picker = document.querySelector("input#datetime");
+const shareButton = document.querySelector("button#share-btn");
 const s_counter = document.querySelector(".container[data-type=seconds] > div.count");
 const s_subtitle = document.querySelector(".container[data-type=seconds] > div.subtitle");
 const m_counter = document.querySelector(".container[data-type=minutes] > div.count");
@@ -22,7 +23,8 @@ function getUrlParameter(name) {
 function queryUpdate(params = {}) {
   var url = new URL(location.href);
   Object.entries(params).forEach(([key, value]) => value ? url.searchParams.set(key, value) : url.searchParams.delete(key));
-  location.href = url.toString();
+  window.history.pushState({}, undefined, url.toString());
+  // location.href = url.toString();
 }
 
 function parseBool(value = "") {
@@ -113,61 +115,116 @@ const monthDifference = (dateFrom, dateTo) => dateTo.getMonth() - dateFrom.getMo
   (12 * (dateTo.getFullYear() - dateFrom.getFullYear()));
 const yearsDifference = (dateFrom, dateTo) => new Date(dateTo - dateFrom).getFullYear() - 1970;
 
-let dateFrom = new Date(parseNum(getUrlParameter("date"), (new Date()).getTime()));
-let prevState = undefined;
 
-const updateContainer = () => {
-  const t1 = new Date();
-  const t2 = new Date(dateFrom);
-  const dif = t1.getTime() - t2.getTime();
-  const seconds = Math.floor(dif / 1000);
-  const minutes = Math.floor(seconds / 60);
-  const hours = Math.floor(minutes / 60);
-  const days = Math.floor(hours / 24);
-  const month = monthDifference(t2, t1);
-  const years = yearsDifference(t2, t1);
-  let nextState = {
-    seconds: seconds,
-    minutes: minutes,
-    hours: hours,
-    days: days,
-    month: month,
-    years: years,
+function fallbackCopyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+
+  // Avoid scrolling to bottom
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+  textArea.style.position = "fixed";
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Fallback: Copying text command was ' + msg);
+  } catch (err) {
+    console.error('Fallback: Oops, unable to copy', err);
   }
-  if (!shallowEqual(prevState, nextState)) {
-    if (prevState?.seconds != nextState.seconds) {
-      s_counter.innerText = nextState.seconds;
-      s_subtitle.innerText = declination(nextState.seconds, ["секунда", "секунды", "секунд"]);
-    }
-    if (prevState?.minutes != nextState.minutes) {
-      m_counter.innerText = nextState.minutes;
-      m_subtitle.innerText = declination(nextState.minutes, ["минута", "минуты", "минут"]);
-    }
-    if (prevState?.hours != nextState.hours) {
-      h_counter.innerText = nextState.hours;
-      h_subtitle.innerText = declination(nextState.hours, ["час", "часа", "часов"]);
-    }
-    if (prevState?.days != nextState.days) {
-      d_counter.innerText = nextState.days;
-      d_subtitle.innerText = declination(nextState.days, ["день", "дня", "дней"]);
-    }
-    if (prevState?.month != nextState.month) {
-      mn_counter.innerText = nextState.month;
-      mn_subtitle.innerText = declination(nextState.month, ["месяц", "месяца", "месяцев"]);
-    }
-    if (prevState?.years != nextState.years) {
-      yy_counter.innerText = nextState.years;
-      yy_subtitle.innerText = declination(nextState.years, ["год", "года", "лет"]);
-    }
-    prevState = nextState;
+
+  document.body.removeChild(textArea);
+}
+function copyTextToClipboard(text) {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
   }
+  navigator.clipboard.writeText(text).then(function() {
+    console.log('Async: Copying to clipboard was successful!');
+  }, function(err) {
+    console.error('Async: Could not copy text: ', err);
+  });
+}
+
+{
+
+  var dateFrom = new Date();
+  function InitParams() {
+    dateFrom = new Date(parseNum(getUrlParameter("date"), (new Date()).getTime()));
+    console.debug(">> time query", "->", dateFrom.getTime());
+    picker.value = new Date(dateFrom.getTime() + new Date().getTimezoneOffset() * -60 * 1000).toISOString().slice(0, 19)
+  }
+
+  var prevState = undefined;
+  const updateContainer = () => {
+    const t1 = new Date();
+    const t2 = new Date(dateFrom);
+    const dif = t1.getTime() - t2.getTime();
+    const seconds = Math.floor(dif / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+    const month = monthDifference(t2, t1);
+    const years = yearsDifference(t2, t1);
+    let nextState = {
+      seconds: seconds,
+      minutes: minutes,
+      hours: hours,
+      days: days,
+      month: month,
+      years: years,
+    }
+    if (!shallowEqual(prevState, nextState)) {
+      if (prevState?.seconds != nextState.seconds) {
+        s_counter.innerText = nextState.seconds;
+        s_subtitle.innerText = declination(nextState.seconds, ["секунда", "секунды", "секунд"]);
+      }
+      if (prevState?.minutes != nextState.minutes) {
+        m_counter.innerText = nextState.minutes;
+        m_subtitle.innerText = declination(nextState.minutes, ["минута", "минуты", "минут"]);
+      }
+      if (prevState?.hours != nextState.hours) {
+        h_counter.innerText = nextState.hours;
+        h_subtitle.innerText = declination(nextState.hours, ["час", "часа", "часов"]);
+      }
+      if (prevState?.days != nextState.days) {
+        d_counter.innerText = nextState.days;
+        d_subtitle.innerText = declination(nextState.days, ["день", "дня", "дней"]);
+      }
+      if (prevState?.month != nextState.month) {
+        mn_counter.innerText = nextState.month;
+        mn_subtitle.innerText = declination(nextState.month, ["месяц", "месяца", "месяцев"]);
+      }
+      if (prevState?.years != nextState.years) {
+        yy_counter.innerText = nextState.years;
+        yy_subtitle.innerText = declination(nextState.years, ["год", "года", "лет"]);
+      }
+      prevState = nextState;
+    }
+    window.requestAnimationFrame(updateContainer);
+  };
+
+  picker.addEventListener("change", (e) => {
+    const value = e.target.value;
+    dateFrom = new Date(value);
+    console.debug(">> time pick", "->", dateFrom.getTime());
+  });
+
+  shareButton.addEventListener("click", (e) => {
+    console.debug(">> time share", "->", dateFrom.getTime());
+    queryUpdate({ date: dateFrom.getTime() });
+    copyTextToClipboard(location.href);
+  });
+
+  window.addEventListener("popstate", (e) => {
+    InitParams();
+  });
+
+  InitParams();
   window.requestAnimationFrame(updateContainer);
-};
-
-picker.addEventListener("change", (e) => {
-  const value = e.target.value;
-  console.debug(value);
-  dateFrom = value;
-});
-picker.value = new Date(dateFrom.getTime() + new Date().getTimezoneOffset() * -60 * 1000).toISOString().slice(0, 19)
-window.requestAnimationFrame(updateContainer);
+}
